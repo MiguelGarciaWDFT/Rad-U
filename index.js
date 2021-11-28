b92c98b3e8ffa74bdb66bfa2509d91108f83d486
Miguel Garcia <gunsandgamer@hotmail.com>
2021-11-28 00:13:54 -0600

    ignorefunciona

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-//SOLO
+//Importaciones
+const express = require("express")
+const app = express()
+
+const hbs = require("hbs")
+
+const connectDB = require("./config/db")
+
+require("dotenv").config()
+
+//Middlewares
+app.use(express.static("public"))
+
+app.set("views", __dirname + "views")
+app.set("view engine", "hbs")
+
+hbs.registerPartials(__dirname + "/views/partials")
+
+app.use(express.urlencoded({ extended: true }))
+
+connectDB()
+
+//Rutas
+app.use("/users", require("./routes/users"))
+app.use("/", require("./routes/index"))
+
+//Servidor
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor conectado en puerto: ${process.env.PORT}`)
+})
